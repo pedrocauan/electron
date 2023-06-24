9c87422afb54693da699e7d654f2d7f6b9a465ea
pedrocauan <mainyasuo92@gmail.com>
2023-06-24 18:37:12 -0300

    preload e primeira compilação

--- instalacao/index.js
+++ instalacao/index.js
@@ -1,11 +1,18 @@
 const { app, BrowserWindow } = require("electron")
+const path = require("path")
+
+
 
 //função que cria a janela
 const createWindow = () => {
     //Objeto com a nova janela
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        //carrega preload
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
     })
 
     //carrega o arquivo html em uma janela
@@ -15,4 +22,16 @@ const createWindow = () => {
 // as janelas só sao criadas após o modulo APP Disparar o evento ready.
 app.whenReady().then(() => {
     createWindow();
+
+    //Cria janela no macos
+    app.on("activate", () => {
+        //abre a janela no macos
+        if(BrowserWindow.getAllWindows.length === 0)
+            createWindow()
+    })
+})
+
+app.on("window-all-closed", () => {
+    if(process.platform !== "darwin")
+        app.quit()
 })
